ba819de78bcf67fb4417f0b92226b1a34d818834
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-25 14:22:14 -0400

    Initial prompts

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+​employeeDirectory => {
+    inquirer.prompt([
+        {type: "input", name: "name", message: "What is the Employee's name?"},
+        {type: "input", name: "id", message: "Please enter their Employee ID."},
+        {type: "input", name: "email", meassage: "Please enter the Employee's E-Mail."},
+        {type: "list", name: "role", message: "What role does the Employee have?", choices: ["Engineer", "Manager", "Intern"]}
+    ]);
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
